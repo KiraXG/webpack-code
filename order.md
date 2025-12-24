39b38750675c3336dee4955a2951b09a2c0b6e61
KiraXG <2629548748@qq.com>
2025-12-24 17:50:14 +0800

    first commit

---
+++ order.md
@@ -0,0 +1,4 @@
+# npm i webpack webpack-cli `基础依赖`
+# npx webpack ./src/main.js --mode=development *指定文件入口并打包为`开发环境`*
+# npx webpack ./src/main.js --mode=production  `生产环境`
+# npx webpack `打包资源`
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+// 配置项补全提示
+/**
+ * @type {import('webpack').Configuration}
+ */
+
+const path = require("path"); // nodejs核心模块，专门用来处理路径问题的
+
+module.exports = {
+    // 入口
+    entry: "./src/main.js",
+    // 输出
+    output: {
+        // 文件输出路径
+        // __dirname nodejs变量，代表当前文件的文件夹目录
+        path: path.resolve(__dirname, "dist"), // 绝对路径
+        // 文件名
+        filename: "main.js",
+    },
+    // 加载器
+    module: {
+        rules: [
+            // loader的配置
+            /* 样式 */
+            {
+                test: /\.css$/, // 只检测.css文件
+                use: [
+                    // 执行顺序是从右往左（从下往上）
+                    "style-loader", // 将js中css通过创建style标签添加到html文件中并生效
+                    "css-loader", // 将css资源编译成commonjs的模块到js中
+                ]
+            },
+            {
+                test: /\.less$/,
+                // loader: "xxx", // 一个loader用‘loader’
+                use: [
+                    // 多个loader用‘use’
+                    "style-loader",
+                    "css-loader",
+                    "less-loader", // 将less文件编译成less文件
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/, // 同时处理sass和scss文件
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader", // 将sass文件编译成less文件
+                ]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "stylus-loader", // 将sass文件编译成less文件
+                ]
+            },
+            /* 图片 */
+            {
+                test: /\.(png|jpe?g|gif|webp|svg)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        // 小于 10kb 的图片转 base64
+                        /* 
+                           优点：减少请求数量
+                           缺点：体积会更大
+                        */
+                        maxSize: 10 * 1024
+                    }
+                }
+            }
+        ],
+    },
+    // 插件
+    plugins: [
+        // plugin的配置
+    ],
+    // 模式
+    mode: "development",
+};

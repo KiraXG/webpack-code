df21fa5f4e8915106ec357d79fb7a92a7e1f2d0a
KiraXG <2629548748@qq.com>
2026-01-22 08:38:58 +0800

    HMR热更新

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -118,7 +118,8 @@ module.exports = {
     devServer: {
         host: "localhost", // 启动服务器域名
         port: "3000", // 启动服务器端口号
-        open: true // 是否自动打开浏览器
+        open: true, // 是否自动打开浏览器
+        hot: true, // 启用HMR，默认为true
     },
     // 模式
     mode: "development",
--- src/main.js
+++ src/main.js
@@ -10,3 +10,10 @@ import "./stylus/index.styl";
 
 console.log(count(2, 1));
 console.log(sum(1, 2, 3, 4));
+
+// css天然支持热更新，js不支持，需要判断
+if (module.hot) {
+    // 判断是否支持热模块替换功能
+    module.hot.accept(); // 不传参默认匹配所有js文件
+    // module.hot.accept("./js/count.js");
+}
